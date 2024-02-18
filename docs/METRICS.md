57ed5c237d2a18b9f4516c955e622896b55ecf5b
pe4eniks <evgeniipishchik@mail.ru>
2024-02-18 12:25:48 +0300

    metrics calculation for pretrained and finetuned RealESRGAN_x4plus

--- docs/METRICS.md
+++ docs/METRICS.md
@@ -4,15 +4,15 @@
 
 ### RealESRGAN_x4plus.yaml
 
-|   Project Type   |  LR  |  HR   | PSNR 🡑 | SSIM 🡑 | LPIPS 🡑 |
-|:----------------:|:----:|:-----:|:-------:|:-------:|:--------:|
-| Game Engine Data | 270p | 1080p | 24.777  |  0.822  |  0.396   |
-|  Downscale Data  | 270p | 1080p |         |         |          |
+|   Project Type   |  LR  |  HR   | PSNR 🡑 | SSIM 🡑 | FSIM 🡑 | LPIPS 🡑 | HaarPSI 🡑 |
+|:----------------:|:----:|:-----:|:-------:|:-------:|:-------:|:--------:|:----------:|
+| Game Engine Data | 270p | 1080p | 23.5409 | 0.7992  | 0.8801  |  0.3924  |   0.5158   |
+|  Downscale Data  | 270p | 1080p |         |         |         |          |            |
 
 
 ### Finetuned_RealESRGAN_x4plus.yaml
 
-|   Project Type   |  LR  |  HR   | PSNR 🡑 | SSIM 🡑 | LPIPS 🡑 |
-|:----------------:|:----:|:-----:|:-------:|:-------:|:-------:|
-| Game Engine Data | 270p | 1080p |   |   |         |
-|  Downscale Data  | 270p | 1080p |   |    |         |
+|   Project Type   |  LR  |  HR   | PSNR 🡑 | SSIM 🡑 | FSIM 🡑 | LPIPS 🡑 | HaarPSI 🡑 |
+|:----------------:|:----:|:-----:|:-------:|:-------:|:-------:|:--------:|:----------:|
+| Game Engine Data | 270p | 1080p | 24.3873 | 0.8348  | 0.8928  |  0.3009  |   0.5625   |
+|  Downscale Data  | 270p | 1080p |         |         |         |          |            |
--- metric/metric_inference.py
+++ metric/metric_inference.py
@@ -1,51 +1,77 @@
-import csv
+import json
 import logging
 import os
 import sys
+from argparse import ArgumentParser
 from datetime import datetime
-from pathlib import Path
+from pathlib import Path, PurePath
 
 import cv2
 import numpy as np
 import torch
 from metric_class import MetricSR
-from options import get_options
 
 import datasets
 import mlflow
 
+sys.path.insert(0, str(Path(__file__).parents[1]))
+from model.emt_model import configure as configure_emt
+from model.emt_model import predict as predict_emt
+from model.real_esrgan import configure as configure_real_esrgan
+from model.real_esrgan import predict as predict_real_esrgan
+from model.resshift import configure as configure_resshift
+from model.resshift import predict as predict_resshift
+from utils.parse import parse_yaml
 
-def main() -> None:
+
+def calculate_metrics(
+    root: PurePath,
+    model_config: str,
+    model_type: str,
+    dataset_name: str,
+    dataset_type: str,
+    split: str,
+    lr: str,
+    hr: str,
+    mlflow_tracking_uri: str,
+    mlflow_experiment: str,
+    mlflow_run: str,
+    mlflow_system_metrics: bool,
+) -> None:
     """
-    Вычисление, запись в файл метрик metrics.csv, логирование в MLFlow.
-    В качестве датасета используется один из проектов из
-    epishchik/super-resolution-games с HuggingFace.
+    Вычисление метрик.
+
+    Parameters
+    ----------
+    root : PurePath
+        Корень проекта.
+    model_config : str
+        Название конфигурационного файла модели.
+    model_type : str
+        Тип модели [pretrained / finetuned].
+    dataset_name : str
+        Название датасета.
+    dataset_type : str
+        Тип датасета [game_engine / downscale].
+    split : str
+        Сплит датасета [train / val].
+    lr : str
+        Низкое разрешение [r270p / r360p / r540p / r1080p]
+    hr : str
+        Высокое разрешение [r270p / r360p / r540p / r1080p]
+    mlflow_tracking_uri : str
+        Адрес трекера MLFlow.
+    mlflow_experiment : str
+        Название эксперимента MLFlow.
+    mlflow_run : str
+        Название запуска MLFlow.
+    mlflow_system_metrics : bool
+        Логирование системных метрик.
 
     Returns
     -------
     None
     """
-    root = Path(__file__).parents[1]
-    sys.path.insert(0, str(root))
-
-    from model.emt_model import configure as configure_emt
-    from model.emt_model import predict as predict_emt
-    from model.real_esrgan import configure as configure_real_esrgan
-    from model.real_esrgan import predict as predict_real_esrgan
-    from model.resshift import configure as configure_resshift
-    from model.resshift import predict as predict_resshift
-    from utils.parse import parse_yaml
-
-    metric_file_header = [
-        "time",
-        "sr_model",
-        "project_type",
-        "project_name",
-        "lr",
-        "hr",
-        "split",
-    ]
-
     map_metric_names = {
         "psnr": "psnr",
         "ssim": "ssim",
@@ -63,62 +89,49 @@ def main() -> None:
         "lpips": "lpips",
         "dists": "dists",
     }
-
     map_no_ref_metric_names = {"brisque": "brisque", "tv": "total_variation"}
 
     for metric_name, map_metric_name in map_no_ref_metric_names.items():
         map_metric_names[metric_name] = map_metric_name
     metric_names = [k for k in map_metric_names.keys()]
     no_ref_metric_names = [k for k in map_no_ref_metric_names.keys()]
-    metric_file_header += metric_names
 
-    args = get_options().parse_args()
-    model_name = args.model_config
+    model_config_path = root / f"configs/model/{model_type}/{model_config}.yaml"
+    metric_config_path = root / f"configs/metric/{dataset_type}/{dataset_name}.yaml"
 
-    model_config_path = root / f"configs/model/{args.model_type}/{model_name}.yaml"
+    model_config_dct = parse_yaml(str(model_config_path))
+    metric_config_dct = parse_yaml(str(metric_config_path))
 
-    metric_config_path = (
-        root / f"configs/metric/{args.dataset_type}/{args.dataset}.yaml"
+    save_dir = root / metric_config_dct["output_path"]
+    os.makedirs(save_dir, exist_ok=True)
+    project_type, project_name = metric_config_dct["project_name"].split("_")
+    save_path = (
+        save_dir / f"{model_config}_{model_type}_{dataset_name}"
+        f"_{dataset_type}_{split}_{lr}_{hr}.json"
     )
 
-    model_config = parse_yaml(str(model_config_path))
-    metric_config = parse_yaml(str(metric_config_path))
-
-    save_dir = root / metric_config["output_path"]
-    os.makedirs(save_dir, exist_ok=True)
-    project_type, project_name = metric_config["project_name"].split("_")
-    save_path = save_dir / "metrics.csv"
-
-    if not os.path.exists(save_path):
-        with open(save_path, "w") as csv_file:
-            csv_writer = csv.writer(csv_file)
-            csv_writer.writerow(metric_file_header)
-
-    if model_config["model"] == "real_esrgan":
-        upsampler = configure_real_esrgan(root, model_config)
-    elif model_config["model"] == "resshift":
-        upsampler = configure_resshift(root, model_config)
-    elif model_config["model"] == "emt":
-        upsampler, emt_device, emt_nbits = configure_emt(root, model_config)
+    if model_config_dct["model"] == "real_esrgan":
+        upsampler = configure_real_esrgan(root, model_config_dct)
+    elif model_config_dct["model"] == "resshift":
+        upsampler = configure_resshift(root, model_config_dct)
+    elif model_config_dct["model"] == "emt":
+        upsampler, emt_device, emt_nbits = configure_emt(root, model_config_dct)
     else:
-        raise ValueError(f"{model_config['model']} incorrect model type.")
+        raise ValueError(f"{model_config_dct['model']} incorrect model type.")
 
-    split_config = args.split
-    if split_config == "train":
-        split = datasets.Split.TRAIN
-    elif split_config == "val":
-        split = datasets.Split.VALIDATION
+    if split == "train":
+        dataset_split = datasets.Split.TRAIN
+    elif split == "val":
+        dataset_split = datasets.Split.VALIDATION
     else:
-        raise ValueError(f"{split_config} does not exist")
+        raise ValueError(f"{split} does not exist")
 
     dataset = datasets.load_dataset(
-        metric_config["repository"],
-        name=metric_config["project_name"],
-        split=split,
+        metric_config_dct["repository"],
+        name=metric_config_dct["project_name"],
+        split=dataset_split,
         streaming=True,
     )
-
-    lr, hr = args.lr, args.hr
     metric_calculator = MetricSR(metric_names, no_ref_metric_names, map_metric_names)
 
     logger = logging.getLogger()
@@ -138,25 +151,24 @@ def main() -> None:
 
     logger.setLevel("INFO")
 
-    logger.info(f"model = {model_name}")
+    logger.info(f"model = {model_config}")
     logger.info(f"project type = {project_type}")
     logger.info(f"project name = {project_name}")
     logger.info(f"low resolution = {lr}")
     logger.info(f"high resolution = {hr}")
-    logger.info(f"split = {split_config}")
+    logger.info(f"split = {split}")
+    logger.info(f"save_path = {save_path}")
 
-    if args.mlflow_tracking_uri:
-        mlflow.set_tracking_uri(args.mlflow_tracking_uri)
-        if not mlflow.get_experiment_by_name(args.mlflow_experiment):
-            mlflow.create_experiment(args.mlflow_experiment)
-        mlflow.set_experiment(args.mlflow_experiment)
+    if mlflow_tracking_uri:
+        mlflow.set_tracking_uri(mlflow_tracking_uri)
+        if not mlflow.get_experiment_by_name(mlflow_experiment):
+            mlflow.create_experiment(mlflow_experiment)
+        mlflow.set_experiment(mlflow_experiment)
 
-        mlflow.start_run(
-            run_name=args.mlflow_run, log_system_metrics=args.mlflow_system_metrics
-        )
+        mlflow.start_run(run_name=mlflow_run, log_system_metrics=mlflow_system_metrics)
 
-        mlflow.log_params({f"model_{k}": v for k, v in model_config.items()})
-        mlflow.log_params({f"metric_{k}": v for k, v in metric_config.items()})
+        mlflow.log_params({f"model_{k}": v for k, v in model_config_dct.items()})
+        mlflow.log_params({f"metric_{k}": v for k, v in metric_config_dct.items()})
         mlflow.log_params(
             {
                 "low resolution": lr,
@@ -175,16 +187,16 @@ def main() -> None:
         bgr_hr = cv2.cvtColor(rgb_hr, cv2.COLOR_RGB2BGR)
 
         outscale = int(bgr_hr.shape[0] / bgr_lr.shape[0])
-        if model_config["model"] == "real_esrgan":
+        if model_config_dct["model"] == "real_esrgan":
             res_hr = predict_real_esrgan(
-                bgr_lr, upsampler, outscale, model_config["use_face_enhancer"]
+                bgr_lr, upsampler, outscale, model_config_dct["use_face_enhancer"]
             )
-        elif model_config["model"] == "resshift":
+        elif model_config_dct["model"] == "resshift":
             res_hr = predict_resshift(bgr_lr, upsampler)
-        elif model_config["model"] == "emt":
+        elif model_config_dct["model"] == "emt":
             res_hr = predict_emt(bgr_lr, upsampler, emt_device, emt_nbits)
         else:
-            raise ValueError(f"{model_config['model']} incorrect model type.")
+            raise ValueError(f"{model_config_dct['model']} incorrect model type.")
 
         res_hr, bgr_hr = torch.from_numpy(res_hr), torch.from_numpy(bgr_hr)
         res_hr = res_hr.unsqueeze(0).permute(0, 3, 1, 2) / 255.0
@@ -200,35 +212,78 @@ def main() -> None:
         for metric_name in metric_names:
             metric_val = metric_calculator.metric_history[metric_name][-1]
             metric_str += f"{metric_name} = {metric_val:.3f}, "
-            if args.mlflow_tracking_uri:
+            if mlflow_tracking_uri:
                 mlflow.log_metric(metric_name, metric_val, step=idx)
         logger.info(metric_str[:-2])
 
-    metrics_total = []
+    json_dct = {
+        "cli_args": {
+            "model_config": model_config,
+            "model_type": model_type,
+            "dataset_name": dataset_name,
+            "dataset_type": dataset_type,
+            "split": split,
+            "lr": lr,
+            "hr": hr,
+            "mlflow_tracking_uri": mlflow_tracking_uri,
+            "mlflow_experiment": mlflow_experiment,
+            "mlflow_run": mlflow_run,
+            "mlflow_system_metrics": mlflow_system_metrics,
+        },
+        "model_config": model_config_dct,
+        "metric_config": metric_config_dct,
+        "metrics": {},
+    }
+
     for metric_name in metric_names:
         metric_val = metric_calculator.calculate_total(metric_name)
-        metrics_total += [f"{metric_val:.3f}"]
-        if args.mlflow_tracking_uri:
+        json_dct["metrics"][metric_name] = float(f"{metric_val:.4f}")
+        if mlflow_tracking_uri:
             mlflow.log_metric(f"mean {metric_name}", metric_val)
-    if args.mlflow_tracking_uri:
+    if mlflow_tracking_uri:
         mlflow.end_run()
+    json_dct["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    with open(save_path, "a") as csv_file:
-        csv_writer = csv.writer(csv_file)
-        time = datetime.now()
-        csv_writer.writerow(
-            [
-                time.strftime("%Y-%m-%d %H:%M:%S"),
-                model_name,
-                project_type,
-                project_name,
-                lr[1:],
-                hr[1:],
-                split_config,
-                *metrics_total,
-            ]
-        )
+    with open(save_path, "w") as f:
+        json.dump(json_dct, f, indent=4)
 
 
 if __name__ == "__main__":
-    main()
+    parser = ArgumentParser()
+    # TODO добавить help
+    parser.add_argument("--model-config", type=str, required=True)
+    parser.add_argument(
+        "--model-type", type=str, required=True, choices=["pretrained", "finetuned"]
+    )
+    parser.add_argument("--dataset-name", type=str, required=True)
+    parser.add_argument(
+        "--dataset-type", type=str, required=True, choices=["downscale", "game_engine"]
+    )
+    parser.add_argument("--split", type=str, required=True, choices=["train", "val"])
+    parser.add_argument(
+        "--lr", type=str, required=True, choices=["r270p", "r360p", "r540p", "r1080p"]
+    )
+    parser.add_argument(
+        "--hr", type=str, required=True, choices=["r270p", "r360p", "r540p", "r1080p"]
+    )
+    parser.add_argument("--mlflow-tracking-uri", type=str, default=None)
+    parser.add_argument("--mlflow-experiment", type=str, default="SRGB Inference")
+    parser.add_argument("--mlflow-run", type=str, default=None)
+    parser.add_argument("--mlflow-system-metrics", action="store_true")
+    args = parser.parse_args()
+
+    root = Path(__file__).parents[1]
+    calculate_metrics(
+        root,
+        args.model_config,
+        args.model_type,
+        args.dataset_name,
+        args.dataset_type,
+        args.split,
+        args.lr,
+        args.hr,
+        args.mlflow_tracking_uri,
+        args.mlflow_experiment,
+        args.mlflow_run,
+        args.mlflow_system_metrics,
+    )
